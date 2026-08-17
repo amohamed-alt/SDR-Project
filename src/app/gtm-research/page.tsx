7b241fdaad818931588518f2b053a0ac24be4610
amohamed-alt <a.mohamed@bayt.net>
2026-08-17 18:59:02 +0300

    Add GTM research workspace route

---
+++ src/app/gtm-research/page.tsx
@@ -0,0 +1,7 @@
+import { GTMResearchWorkspace } from "@/components/GTMResearchWorkspace";
+
+export const dynamic = "force-dynamic";
+
+export default function GTMResearchPage() {
+  return <GTMResearchWorkspace />;
+}
