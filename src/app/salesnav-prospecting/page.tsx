a631007fc58129ad0a60b20b80d785dfb2f35ed7
amohamed-alt <a.mohamed@bayt.net>
2026-08-20 14:32:36 +0300

    Add Sales Navigator prospecting page

---
+++ src/app/salesnav-prospecting/page.tsx
@@ -0,0 +1,7 @@
+import { SalesNavProspecting } from "@/components/SalesNavProspecting";
+
+export const dynamic = "force-dynamic";
+
+export default function SalesNavProspectingPage() {
+  return <SalesNavProspecting />;
+}
