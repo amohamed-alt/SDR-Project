8c5616b48efad4c2ec41678778807967f5aec07e
amohamed-alt <a.mohamed@bayt.net>
2026-08-31 17:57:24 +0300

    Delete retired career, hiring, and ATS implementation

--- src/app/hiring/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { HiringIntelligence } from "@/components/HiringIntelligence";
-
-export default function HiringPage() {
-  const router = useRouter();
-  return <HiringIntelligence onBack={() => router.push("/prospecting")} />;
-}
